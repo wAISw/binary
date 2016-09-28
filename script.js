7de47c25bd972e864a0b24037b66dd32446b5cb8
Abdulov Ivan <iabdulov@mera.ru>
2016-09-28 17:41:39 +0300

    update

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 // Создание бинартного дерева
 
 
-function pushToBTree(count, row) {
+function pushToBTree(count, balance, row) {
     var bTree = [];
+
+    count--;
     for (var j = 1; j < 3; j++) {
-        // если число больше 0.5, то создаем ветку
-        if (Math.random() > 0) {
+        // если число больше 0.2, то создаем ветку
+        if (Math.random() > 0.2 || balance) {
             var id = (row || "") + "" + j;
-            count--;
             if (count > 0) {
                 bTree.push({
                     id: id,
-                    children: pushToBTree(count, id)
+                    children: pushToBTree(count, balance, id)
                 });
             }
         }
     }
     return bTree
 }
-bTree = pushToBTree(7);
+bTree = pushToBTree(6, false);
 console.log(bTree);
 
 
 // обход бинарного дерева
 var element = document.createElement('ul');
-function walk(tree, ul) {
+function walkWithView(tree, ul) {
     for (var j = 0; j < 2; j++) {
         if (tree[j]) {
             var element = document.createElement("li");
-            console.log(element);
             element.innerHTML = "<div><span>" + tree[j].id + "</span><div class='arrows'></div></div>";
             var elementUl = document.createElement('ul');
-            element.appendChild(walk(tree[j].children, elementUl));
+            element.appendChild(walkWithView(tree[j].children, elementUl));
             ul.appendChild(element);
-            console.log(tree[j].id);
         }
     }
     return ul;
 }
-document.getElementById('main').appendChild(walk(bTree, element));
+
+function walk(tree) {
+    for (var j = 0; j < 2; j++) {
+        if (tree[j]) {
+            console.log(tree[j].id);
+            walk(tree[j].children);
+        }
+    }
+}
+walk(bTree);
+document.getElementById('main').appendChild(walkWithView(bTree, element));
+
